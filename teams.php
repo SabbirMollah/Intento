54ad8594c3482497e29025dc29754c6675b68c60
Afsana Reevu <reevuafsana@gmail.com>
2020-09-16 15:20:46 +0600

    created the list to show the teams user belongs to

--- teams.php
+++ teams.php
@@ -42,6 +42,22 @@
             </section>
             <section>
                 <h2>Teams I belong to</h2>
+                <?php
+                    require "includes/connect_db.php";
+
+                    $sql = 'SELECT team_name, team_description FROM teams, belongs_to WHERE teams.team_id = belongs_to.team_id AND user_email="'. $_SESSION['email'] .'"';
+                    $result = $conn->query($sql);
+
+                    if ($result->num_rows > 0) {
+                    // output data of each row
+                    while($row = mysqli_fetch_assoc($result)) {
+                    echo "Team Name: " . $row["team_name"]. " - Description: " . $row["team_description"] . "<br>";
+                        }
+                    } 
+                    else {
+                            echo "You don't belong to any teams!";
+                        }
+                ?>
 
             </section>
         </div>
